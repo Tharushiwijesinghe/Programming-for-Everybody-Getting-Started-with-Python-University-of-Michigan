c97920b4013f3edec7e9559ae1a100a8c525cb08
Tharushiwijesinghe <134124219+Tharushiwijesinghe@users.noreply.github.com>
2023-11-26 10:38:34 +0530

    Create Ass2.3.py

---
+++ Assignments/Ass2.3.py
@@ -0,0 +1,11 @@
+# This first line is provided for you
+
+hrs = input("Enter Hours:")
+rate = input("Enter rate per hour")
+
+hrs_f = float(hrs)
+rate_f = float(rate)
+
+pay =hrs_f * rate_f
+
+print('Pay:',pay)
