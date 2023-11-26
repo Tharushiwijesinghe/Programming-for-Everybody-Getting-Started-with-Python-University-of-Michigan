d045f2dae4f688b533e1c1c0577c3b422e30c89e
Tharushiwijesinghe <134124219+Tharushiwijesinghe@users.noreply.github.com>
2023-11-26 09:01:55 +0530

    Create type.py

---
+++ Types/type.py
@@ -0,0 +1,9 @@
+ddd = 1 + 4
+print(ddd)
+
+eee = 'hello' + 'there'
+print(eee)
+
+print(type('eee'))   # Output: <class 'str'>
+print(type('hello')) # Output: <class 'str'>
+print(type(1))        # Output: <class 'int'>
