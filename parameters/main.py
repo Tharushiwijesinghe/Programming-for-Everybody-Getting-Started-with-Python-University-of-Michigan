d6834af84fc686a9b0b016148c51a672c7b40cc5
Tharushiwijesinghe <134124219+Tharushiwijesinghe@users.noreply.github.com>
2023-11-27 21:08:30 +0530

    Create main.py

---
+++ parameters/main.py
@@ -0,0 +1,10 @@
+def greet(lang):
+    if lang == 'es':
+        print('Hola')
+    elif lang == 'fr':
+        print('Bonjur')
+    else:
+        print('Hello')
+greet('en')      
+greet('es')
+greet('fr') 
