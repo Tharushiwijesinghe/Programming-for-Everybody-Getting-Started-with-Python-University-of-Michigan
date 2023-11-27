2f87bd52ea6b60b8d9bb99c798da28ccb0b2c331
Tharushiwijesinghe <134124219+Tharushiwijesinghe@users.noreply.github.com>
2023-11-27 21:53:38 +0530

    Create rvalue.py

---
+++ Return values/rvalue.py
@@ -0,0 +1,10 @@
+def greet(lang):
+    if lang == 'es':
+        return 'Hola'
+    elif lang == 'fr':
+        return'Bonjur'
+    else:
+        return 'Hello'
+print(greet('en'),'tharu')      
+print(greet('es'),'Dew')
+print(greet('fr'),'Shehan')
