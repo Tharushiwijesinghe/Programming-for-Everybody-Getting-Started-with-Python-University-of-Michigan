c8a98faccd5a0a5adb63141f53fd209c5e9135c4
Tharushiwijesinghe <134124219+Tharushiwijesinghe@users.noreply.github.com>
2023-11-27 07:19:44 +0530

    Create Multy-way.py

---
+++ More_Conditional_Statements/Multy-way.py
@@ -0,0 +1,16 @@
+x=4
+if x<2:
+    print('Ssmall')
+elif x<10:
+    print('Medium')
+else:
+    print('LARGE')
+print('All Done')
+
+#no else
+x=0
+if x<2:
+    print('Ssmall')
+elif x<10:
+    print('Medium')
+print('All Done')
