06367365c4f1f96bcbaa643f79eaffff8a91928e
Tharushiwijesinghe <134124219+Tharushiwijesinghe@users.noreply.github.com>
2023-11-27 06:21:37 +0530

    Create One_way decision.py

---
+++ Conditional_Statements/One_way decision.py
@@ -0,0 +1,13 @@
+x=5
+print('Before 5')
+if x==5:
+    print('is  5')
+    print('is stil 5')
+    print('Third  5')
+print('Afterwards  5')
+print('Before 6')
+if x ==6:
+    print('is  6')
+    print('is stil 6')
+    print('Third  6')
+print('Afterwards  6')
