9b869ac4b5729911cd819e239659624ddb46880f
Tharushiwijesinghe <134124219+Tharushiwijesinghe@users.noreply.github.com>
2023-11-29 08:05:37 +0530

    Create Ass4.1.py

---
+++ Assignments/Ass4.1.py
@@ -0,0 +1,17 @@
+def computepay(h, r):
+    
+    if h <= 40:
+       pay = h*r
+    
+    else:
+        pay= ((h-40)*r*1.5) + (40*r)
+    return pay
+
+hrs = input("Enter Hours:")
+h = float(hrs)
+
+rate = input("input rate:")
+r = float(rate)
+
+p = computepay(h, r)
+print("Pay", p)
